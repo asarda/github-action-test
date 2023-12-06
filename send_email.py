33ffc6785522b3546d9d8c6072c45c8ddcfd4665
albsarda <albert.sarda@grupoadslzone.com>
2023-12-06 19:40:58 +0100

    send email script & requirements

---
+++ send_email.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+from email.message import EmailMessage
+import os
+import ssl
+import smtplib
+
+load_dotenv()
+email_sender = os.environ["EMAIL_SENDER"]
+email_password = os.environ["EMAIL_PASSWORD"]
+email_reciever = 'asarda@mailinator.com'
+
+subject = "Sent from github actions"
+body = "Hello!"
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_reciever
+em['subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender,email_password)
+    smtp.sendmail(email_sender, email_reciever, em.as_string())
+
+print("Email sent")
