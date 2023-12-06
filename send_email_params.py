094a1f6209effe2830c57fb0fe7e9990fcbab509
albsarda <albert.sarda@grupoadslzone.com>
2023-12-06 21:04:05 +0100

    send email params file, comment send email

--- send_email_params.py
+++ send_email_params.py
@@ -19,8 +19,8 @@ em['subject'] = subject
 em.set_content(body)
 
 context = ssl.create_default_context()
-#with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-#    smtp.login(email_sender,email_password)
-#    smtp.sendmail(email_sender, email_reciever, em.as_string())
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender,email_password)
+    smtp.sendmail(email_sender, email_reciever, em.as_string())
 
-print("Email sent (not really #)")
+print("Email sent")
